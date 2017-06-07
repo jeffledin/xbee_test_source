f29b9609638c5c8cb5c7ce065d74a4b9476be49b
jeffledin <ledin.jeffrey@gmail.com>
2017-06-07 00:36:10 -0500

    Revert "cleanup"
    This reverts commit 66f476a958e74735e49b28ee87aef7d897944072.

--- isr.c
+++ isr.c
@@ -6,6 +6,32 @@
 volatile unsigned char receiveFlag = 0;
 volatile struct bufferStruct receiveBuffer;
 
+//void __ISR(0, ipl1) isr() // single vector
+//{
+//    if(IFS0bits.T2IF)
+//    {
+//        IFS0bits.T2IF = 0;
+//        printf("Heya\n");
+//    }
+//}
+
+//void __ISR(_TIMER_2_VECTOR, ipl1) T2Interrupt() // multi-vector
+//{
+//    unsigned static int thirtySeconds = 0;
+//    IFS0bits.T2IF = 0;
+//    
+//    if(thirtySeconds >= 300)
+//    {
+//        thirtySeconds = 0;
+//        readTemperatureFlag = 1;
+//        printf("Reading temperature...\n");
+//    }
+//    else
+//    {
+//        ++thirtySeconds;
+//    }
+//}
+
 void __ISR(_UART_1_VECTOR, ipl1) UART1Interrupt()
 {   
     IFS0bits.U1RXIF = 0;
