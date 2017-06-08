0d25cf242b31dfd1bf4a8442e1f0d4d88857121c
jeffledin <ledin.jeffrey@gmail.com>
2017-06-07 23:10:13 -0500

    N/A

--- isr.c
+++ isr.c
@@ -8,40 +8,32 @@ volatile struct bufferStruct receiveBuffer;
 
 void __ISR(_UART_1_VECTOR, ipl1) UART1Interrupt()
 {   
-    unsigned static char byteCount = 0;
-    
-    if(IFS0bits.U1RXIF)
+
+    IFS0bits.U1RXIF = 0;
+
+    if(U1STAbits.URXDA) // IMPORTANT: why do we get two interrupts per byte?
     {
-        IFS0bits.U1RXIF = 0;
-        
-        if(U1STAbits.URXDA) // IMPORTANT: why do we get two interrupts per byte?
+        receiveBuffer.buffer[receiveBuffer.currentIndex] = U1RXREG;
+        //printf("0x%.2X\n", receiveBuffer.buffer[receiveBuffer.currentIndex]);
+
+        switch(operatingMode)
         {
-            receiveBuffer.buffer[receiveBuffer.currentIndex] = U1RXREG;
-            //printf("0x%.2X\n", receiveBuffer.buffer[receiveBuffer.currentIndex]);
-
-            switch(operatingMode)
-            {
-                case AT_MODE:
-                    if(receiveBuffer.buffer[receiveBuffer.currentIndex] == '\r') receiveFlag = 1; 
-                    break;
-                case API_MODE_NO_ESCAPE:
-        //            if(receiveBuffer.buffer[receiveBuffer.currentIndex] == 0x7E) byteCount = 0;//printf("Open response flag\n");
-                    printf("0x%.2X\n", receiveBuffer.buffer[receiveBuffer.currentIndex]);
-                    break;
-                case API_MODE_WITH_ESCAPE:
-                    break;
-                default:
-                    break;
-            }
-
-            ++receiveBuffer.numBytes;
-            ++receiveBuffer.currentIndex;
+            case AT_MODE:
+                if(receiveBuffer.buffer[receiveBuffer.currentIndex] == '\r') receiveFlag = 1; 
+                break;
+            case API_MODE_NO_ESCAPE:
+    //            if(receiveBuffer.buffer[receiveBuffer.currentIndex] == 0x7E) byteCount = 0;//printf("Open response flag\n");
+                printf("0x%.2X\n", receiveBuffer.buffer[receiveBuffer.currentIndex]);
+                break;
+            case API_MODE_WITH_ESCAPE:
+                break;
+            default:
+                break;
         }
 
-        
+        ++receiveBuffer.numBytes;
+        ++receiveBuffer.currentIndex;
     }
-    
-    
 }
 
 
